6dcf4ff69c0e88fe38bb1e110f83e0967f6fe985
DAVI SIMPLÍCIO <davi.simplicio@catolicasc.edu.br>
2024-09-20 21:35:43 -0300

    first commit

---
+++ src/lib/Components/quadroComTexto.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let valor
+</script>
+
+<div class="w-10 h-5 bg-[#494949]">
+    {valor}
+</div>
---
+++ src/routes/Teste/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+
+    let nome = "Davi"
+    let items = ["Peru","Brazil", "Kuba"]
+</script>
+
+
+<div>
+    {#if nome == "Davi"}
+        Bem vindo Davi
+    {/if}
+    {#each items as item}
+        <div class="flex flex-col">
+            {item}
+        </div>
+    {/each}
+    <quadroComTexto valor="Variavel"></quadroComTexto>
+    <quadroComTexto valor="Texto"></quadroComTexto>
+
+</div>
