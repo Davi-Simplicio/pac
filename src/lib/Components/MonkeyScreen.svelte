d0cfc807cf2b7b5a562e6df6e4f0d4f854428d7d
VITOR AUGUSTO RINKAWETSKY <vitor.rinkawetsky@catolicasc.edu.br>
2024-10-18 19:51:02 -0300

    correcao banner

--- src/lib/Components/MonkeyScreen.svelte
+++ src/lib/Components/MonkeyScreen.svelte
@@ -3,16 +3,16 @@
 
 <div class="screen">
 	<div>
-		<h1 class="text-white">Chimp Academy</h1>
+		<h1 class="text-white chimpAcademy">Chimp Academy</h1>
 	</div>
 
 	<div class="general-square">
-		<div class="w-[600px] h-[80px] bg-neutral flex justify-center items-end absolute top-48 left-1/2 -translate-x-1/2 z-10">
-            <div class="rounded-full bg-neutral flex justify-center items-center w-[400px] h-[400px] border-4 border-white shadow-[0_0_80px_rgba(255,255,255,0.5)] shadow-white">
+		<div class="w-[600px] h-[80px] bg-neutral flex justify-center items-end absolute top-96 left-1/2 -translate-x-1/2 z-10">
+            <div class="rounded-full bg-neutral flex justify-center items-center w-[400px] h-[400px] border-4 border-white shadow-[0_0_80px_rgba(255,255,255,0.5)] shadow-secondary-content">
                 <img src="src/lib/images/macaco.png" alt="" srcset="">
             </div>
         </div>
-		<div class="rectangle absolute">
+		<div class="rectangle absolute bottom-32">
 			<div class="mascot-div-text">
 				<p class="mascot-text">Esse é o nosso mascote Dev-CaCo</p>
 				<p class="mascot-text">Profissional em C e Lógica de programação</p>
@@ -25,7 +25,7 @@
 
 <style>
 	.screen {
-		@apply flex flex-col justify-center items-center w-full h-[95vh] bg-cover bg-no-repeat;
+		@apply flex flex-col justify-center items-center w-full h-[105vh] bg-cover bg-no-repeat;
 		background-image: url(../images/bg-Monkey.png);
 	}
 
@@ -42,11 +42,16 @@
 	}
 
 	.mascot-text {
-		@apply text-white;
+		@apply text-secondary-content;
 		font-size: x-large !important;
 	}
 
 	.learn-now-btn {
-		@apply bg-primary font-bold py-6 px-24 rounded absolute top-[94%];
+		@apply bg-primary font-bold py-6 px-24 rounded absolute top-[80%];
+	}
+
+	.chimpAcademy{
+		font-size: 50px !important;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
 	}
 </style>
